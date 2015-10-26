e4635fe280c7f012620a3240f018fe304a6eb0d6
Michał Zieliński <michal@zielinscy.org.pl>
2015-10-26 17:15:34 +0100

    improve util/runall.py

--- util/runall.py
+++ util/runall.py
@@ -7,6 +7,7 @@ import sys
 import pty
 import traceback
 import time
+import fcntl
 
 os.environ['PYTHONUNBUFFERED'] = '1'
 
@@ -29,6 +30,7 @@ class PtyPopen():
     def __init__(self, args):
         self.args = args
         pid, master = pty.fork()
+
         if pid == 0:
             try:
                 self._child()
@@ -37,7 +39,10 @@ class PtyPopen():
             finally:
                 os._exit(0)
         else:
-            self.stdout = os.fdopen(master, 'rb', 1)
+            fl = fcntl.fcntl(master, fcntl.F_GETFL)
+            fcntl.fcntl(master, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
+            self.stdout = os.fdopen(master, 'rb', 0)
 
     def _child(self):
         os.execvp(self.args[0], self.args)
@@ -62,25 +67,45 @@ def finish():
         subprocess.call(['sudo', 'kill', '-%d' % sig] + tasks)
         time.sleep(0.3)
 
+def nonblocking_readline(stream):
+    s = []
+    while True:
+        b = stream.read(1)
+        if b is None:
+            break
+        s.append(b)
+        if b == b'\n':
+            break
+    return b''.join(s)
+
 atexit.register(finish)
 
 reset_colors = '\033[0m'
+nonfinished_line = None
 
 while True:
     r, w, x = select.select([ p.stdout for p in procs.values() ], [], [])
     for stream in r:
         name = [ k for k, v in procs.items() if v.stdout == stream ][0]
 
-        data = '[%s] ' % (name.ljust(10), )
-        sys.stdout.buffer.write(data.encode())
-        sys.stdout.buffer.flush()
+        if nonfinished_line != name:
+            if nonfinished_line is not None:
+                sys.stdout.buffer.write(b'\\\n')
+            data = '[%s] ' % (name.ljust(10), )
+            sys.stdout.buffer.write(data.encode())
+            sys.stdout.buffer.flush()
 
-        ret = stream.readline()
+        ret = nonblocking_readline(stream)
 
         if not ret:
             del procs[name]
             ret = r'{exited}'
 
         sys.stdout.buffer.write(ret)
+        if ret.endswith(b'\n'):
+            nonfinished_line = None
+        else:
+            nonfinished_line = name
+
         sys.stdout.buffer.write(reset_colors.encode())
         sys.stdout.buffer.flush()
